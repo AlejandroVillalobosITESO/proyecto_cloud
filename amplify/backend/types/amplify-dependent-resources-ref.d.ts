0b08af4fb36ecb8ff4d52efe05d41b0a99b3bf25
Alejandro Villalobos <is728135@iteso.mx>
2023-11-07 18:44:54 -0600

    amplify CLI

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "proyectocloud7aa149d0": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
