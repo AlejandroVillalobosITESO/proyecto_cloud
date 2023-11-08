907a6b679affc52104f0418bede520377cf34f72
Alejandro Villalobos <is728135@iteso.mx>
2023-11-07 18:16:01 -0600

    testing if I can still push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Mike es un pendejo</h1>
+          <h1>Mike es un guapo</h1>
         </header>
       </div>
   );
